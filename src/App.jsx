082fc083ea4f05d355c8f4bb592bed6f0f19d91d
Munadil16 <munadilxd@gmail.com>
2024-05-26 12:58:33 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-purple-700 p-5 text-center text-2xl">
+        Portfolio Website
+      </h1>
+    </>
+  );
+}
+
+export default App;
